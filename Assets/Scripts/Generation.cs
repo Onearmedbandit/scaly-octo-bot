bb4146d2ada9446006440320a401f83a5b52f1da
Benjamin Clarke <bac351@nyu.edu>
2014-02-19 11:56:50 -0500

    no message

---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generation : MonoBehaviour {
+	public GameObject treeOne;
+	public GameObject treeTwo;
+	public GameObject bush;
+	public GameObject bear;
+	public GameObject flower;
+	public Transform masterSpawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Time.time < 120f){
+
+
+			float randomNumber = Random.Range (0f,100f);
+			if (randomNumber < 26) {
+				GameObject go = Instantiate (treeOne,masterSpawn.transform.position, Quaternion.identity) as GameObject;
+			}
+			else if (randomNumber < 51 && randomNumber > 25) {
+				GameObject go = Instantiate (treeTwo,masterSpawn.transform.position, Quaternion.Euler (-90f,0f,0f)) as GameObject;
+			}
+			else if (randomNumber < 71 && randomNumber > 50) {
+				GameObject go = Instantiate (bush,masterSpawn.transform.position, Quaternion.identity) as GameObject;
+			}
+			else if (randomNumber < 91 && randomNumber > 70) {
+				GameObject go = Instantiate (bear,masterSpawn.transform.position, Quaternion.identity) as GameObject;
+			}
+			else if (randomNumber < 101 && randomNumber > 90) {
+				GameObject go = Instantiate (flower,masterSpawn.transform.position, Quaternion.identity) as GameObject;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LocationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocationController : MonoBehaviour {
+	float x;
+	float z;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		x = Random.Range (0f,50f);
+		z = Random.Range (0f,50f);
+		transform.position = new Vector3 (x,0f,z);
+	}
+}
